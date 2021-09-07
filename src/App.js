7409eb4928f14374ac4542189988066eb1760b54
Aundre Fredericks <dreallenfred@gmail.com>
2021-09-07 15:37:26 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from 'react';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component = {Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
